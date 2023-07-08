ba6a87a926a27c7467df952f7bffb1a45f3f3d45
chachasammy <72380391+chachasammy@users.noreply.github.com>
2023-07-08 10:53:55 +0300

    first Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'',redirectTo:'login',pathMatch:'full'},
+{path:'login',component: LoginComponent},
+{path:'register',component: RegisterComponent},
+{path:'dashboard',component: DashboardComponent},
+{path:'navbar', component: NavbarComponent},
+{path:'**',redirectTo:'login',pathMatch:'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[LoginComponent,RegisterComponent,DashboardComponent,NavbarComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+    production: false,
+
+    // Firebase configuration
+    firebase: {
+         
+            apiKey: "AIzaSyCXnSk6S8KtzJIkOb6S6koBAbVZN3jEjgc",
+            authDomain: "clientdb-e7462.firebaseapp.com",
+            databaseURL: "https://clientdb-e7462-default-rtdb.firebaseio.com",
+            projectId: "clientdb-e7462",
+            storageBucket: "clientdb-e7462.appspot.com",
+            messagingSenderId: "1224111488",
+            appId: "1:1224111488:web:e8610ea74a585b45c83d65",
+            measurementId: "G-27W47JRBJ6"
+          
+    }
+};
