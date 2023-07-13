acc3bb6f0557d772efabd1f75f9ab3405ff4b347
DevRubia <78357146+DevRubia@users.noreply.github.com>
2023-07-13 13:26:11 +0300

    secondCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import{AngularFireModule} from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
-// import { NavbarComponent }  from './navbar/navbar.component';
-// import { LoginComponent } from './component/login/login.component';
-// import { RegisterComponent } from './component/register/register.component';
-// import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { routingComponents } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import all material components
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatTableModule} from '@angular/material/table';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -19,7 +35,26 @@ import { routingComponents } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    BrowserAnimationsModule
+    ,MatButtonModule
+    ,MatIconModule
+    ,MatBadgeModule
+    ,MatToolbarModule
+    ,MatMenuModule
+    ,MatCardModule
+    ,MatGridListModule
+    ,MatFormFieldModule
+    ,MatInputModule
+    ,MatDividerModule
+    ,MatListModule
+    ,MatDialogModule
+    ,MatTableModule
+    ,MatSelectModule
+    ,MatSnackBarModule
+    ,MatPaginatorModule
+    ,MatSortModule
+    ,MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
